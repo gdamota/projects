ddef06429d0748631f95b3ac9448bdb9d5d5f4a0
Glen Damota <glen@reps2.com>
2018-01-22 16:18:55 +0000

    update fizzbuzz

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,12 +1,11 @@
 def fizzbuzz(n):
-
-    if n%3==0 and n%5==0:
+    if n%15 == 0:
         return 'Fizzbuzz'
-    elif n%3==0 and n%5!==0:
+    elif n%3==0:
         return 'Fizz'
-    elif n%5==0 and n%3!==0:
+    elif n%5==0:
         return 'Buzz'
     else:
         str(n)
 
-print "\n".join(fizzbuzz(n) for n in xrange(1, 100))
+print("\n".join(fizzbuzz(n) for n in range(1, 100))
