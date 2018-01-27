666c26bd7f3e6b9373665481e1f93c1da3de8e31
Glen Damota <35322499+gdamota@users.noreply.github.com>
2018-01-27 11:16:11 -0800

    Update musician.py

--- musician.py
+++ musician.py
@@ -1,4 +1,4 @@
-class musician(object):
+class Musician(object):
     
     def __init__(self, sounds):
         self.sounds = sounds
@@ -7,12 +7,12 @@ class musician(object):
         for i in range(length):
             print(self.sounds[i])
             
-class Basinist(musician):
+class Basinist(Musician):
     
     def __init__(self):
         super().__init__(['twang','thrumb','bling'])
         
-class Guitarist(musician):
+class Guitarist(Musician):
     
     def __init__(self):
         super().__init__(['Boink', 'Bow','Boom'])
@@ -21,40 +21,50 @@ class Guitarist(musician):
         print('Be with you in a momemt')
         print('Twoning, Sproing, Splang')
         
-class Drummer(musician):
+class Drummer(Musician):
     
     def __init__(self):
-        super().__init__(['Bop','Bump','Pow')]
-    
-    def solo(self):
+        super().__init__(['Bop','Bump','Pow'])
+
+    def solo(self, count):
         super().solo(3)
         
     def count():
         print("here we go boys")
-        for num in range(1,4)
-        print(num)
+        for num in range(1,4):
+            print(num)
     
     def Combust():
         print('FIRE!!!!')
         
 class Band(object):
-    def __init__(self, musicians = []):
-        self.musicians = musicians
+    def __init__(self):
+        self.the_drummer = None
+        self.musicians = []
         
-    def hire(musician):
-        self.musicians.append(musician)
-    
+        
+    def hire(self, musician):
+        if isinstance(musician, list) == False:
+            musician = [musician]
+            
+        for new_member in musician:
+            if isinstance(musician, Drummer):
+                self.the_drummer = musician
+            self.musicians.append(new_member)
+            
     def fire(musician):
         self.musicians.remove(musician)
     
-    def play(self)
-        self.musicians = players
-        for member in players:
-            if players[member] = 'Drummer':
-                players[member].count()
-            players[member].solo()
+    def play(self):
+        if self.the_drummer != None:
+            self.the_drummer.count()
+        
+        for member in self.musicians:
+            member.solo(1)
 
 john = Guitarist()
 jerry = Basinist()
 jones = Drummer()
-rolling stones = Band(john,jerry,jones)
+rolling_stones = Band()
+rolling_stones.hire([john, jerry, jones])
+rolling_stones.play()
