51cf7459ed9c5a6f2574e42efddb76a16cbd257e
gdamota <gdamota@sandiego.edu>
2021-04-22 10:07:51 -0400

    reorg

---
+++ basic_python_examples/Bicycle.py
@@ -0,0 +1,66 @@
+class Bicycle(object):
+    def __init__(self, model, weight, cost):
+        self.model = model
+        self.weight = weight
+        self.cost = cost
+
+    def getCost(self):
+        return self.cost
+
+
+class BikeShop(object):
+    def __init__(self, name, inventory, margin):
+        self.name = name
+        self.inventory = inventory
+        self.margin = margin
+
+    def printInv(self):
+        for bike in self.inventory:
+            print(bike.model, bike.cost * self.margin)
+
+    def buy(self, customer):
+        for bike in self.inventory:
+            if bike.cost * self.margin <= customer.funds:
+                self.inventory.remove(bike)
+                remaining = customer.funds - (bike.cost * self.margin)
+                customer.bike = bike
+                print(customer.funds, bike.cost * self.margin, remaining, bike.model)
+                return self.inventory
+                return customer.bike
+
+
+class Customer(object):
+    def __init__(self, name, funds, bike=None):
+        self.name = name
+        self.funds = funds
+
+    def afford(self, shop):
+        for bike in shop.inventory:
+            if bike.cost <= self.funds:
+                print(self.name, bike.model, bike.cost)
+
+
+def main():
+    # creating bikes
+    first = Bicycle("Kids", 100, 100)
+    second = Bicycle("Cruiser", 100, 200)
+    third = Bicycle("BMX", 100, 300)
+    fourth = Bicycle("Mountain", 100, 500)
+    fifth = Bicycle("Racer", 100, 750)
+    sixth = Bicycle("Multi-Purpose", 100, 1000)
+    inventory = [first, second, third, fourth, fifth, sixth]
+
+    # creating shop
+    TheShop = BikeShop("The Shop", inventory, 1.2)
+    john = Customer("john", 200)
+    jerry = Customer("john", 500)
+    jones = Customer("john", 1000)
+
+    # testing methods
+    john.afford(TheShop)
+    TheShop.buy(jerry)
+    TheShop.printInv()
+    print("profit = {}".format(jerry.bike.cost * TheShop.margin))
+
+
+main()
