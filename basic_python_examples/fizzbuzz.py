57f910168dba84348cdb96951678184704248024
gdamota <gdamota@sandiego.edu>
2021-04-23 12:16:52 -0400

    syntax

--- basic_python_examples/fizzbuzz.py
+++ basic_python_examples/fizzbuzz.py
@@ -1,12 +1,7 @@
 def fizzbuzz(n):
-    if n%15 == 0:
-        return 'Fizzbuzz'
-    elif n%3==0:
-        return 'Fizz'
-    elif n%5==0:
-        return 'Buzz'
-    else:
-        str(n)
-
-print("\n".join(fizzbuzz(n) for n in range(1, 100))
-
+    if n % 15 == 0:
+        return "Fizzbuzz"
+    elif n % 3 == 0:
+        return "Fizz"
+    elif n % 5 == 0:
+        return "Buzz"
